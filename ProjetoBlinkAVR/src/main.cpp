3d835b158883d861f711dd9055b84b97e0705387
Allan Cedric <allan.py3000@gmail.com>
2020-05-24 12:43:12 -0300

    Adição de alguns comentários

--- ProjetoBlinkAVR/src/main.cpp
+++ ProjetoBlinkAVR/src/main.cpp
@@ -2,17 +2,20 @@
 
 #include <Arduino.h>
 
-#define TIME_LED 1000
+#define TIME_LED 1000 // Tempo de espera para um delay //
 #define PIN_LED (1 << PB5) // Led interno do pino digital 13
 
 int main(void)
 {
+  // ----- Setup ----- //
   DDRB |= PIN_LED;   //Direction registers
   PORTB &= ~PIN_LED; // Port "state" registers
 
+  // ---- Loop ----- //
   while (1)
   {
-    PORTB ^= PIN_LED;
-    _delay_ms(TIME_LED);
+    PORTB ^= PIN_LED; // Mudança automática de estado //
+    _delay_ms(TIME_LED); // Espera uma quantidade de tempo em milisegundos // 
   }
+  return 0;
 }
